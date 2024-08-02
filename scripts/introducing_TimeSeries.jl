9d2be2e7c5ab0775078765e2da22e6dee0e95cfd
Tbugaj17 <tbugaj17@student.aau.dk>
2024-08-02 19:56:52 +0200

    white noise example

--- scripts/introducing_TimeSeries.jl
+++ scripts/introducing_TimeSeries.jl
@@ -7,9 +7,29 @@
 
 # Introducing Time series Analysis. 
 
-# Create a simple Time Series.
-# Import Packages with 'using'
+# Activate packages from Project.toml and import packages to the script with 'using'
 using Pkg
-Pkg.activate("TimeSeries_beginner.jl")  # Activates the environment based on the Project.toml in the current directory
+Pkg.activate(".")  # Activates the environment based on the Project.toml in the current directory
 
-using TimeSeries, DataFrames
+using Random, Plots, Statistics
+
+# White Noise example.
+Random.seed!(1234)
+
+# Generate 500 samples of Gaussian White Noise.
+n = 500
+white_noise = randn(n) 
+
+# Calculate the mean and variance. 
+# For Gaussian White Noise we expect mean close to zero and variance 1.  
+mean_val = mean(white_noise)
+var_val = var(white_noise)
+
+# Print mean and variance.
+println("Mean value:", mean_val)
+println("Variance:", var_val)
+
+# Plot the white Noise.
+plot(white_noise, title="Gaussian White Noise", xlabel="Samples", ylabel="Value", legend=false, color=:blue )
+
+#.......in progress
