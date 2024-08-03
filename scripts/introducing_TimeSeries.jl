e75ba6641cc62f0e9708f63fde58d3d2f6a5cb73
Tbugaj17 <tbugaj17@student.aau.dk>
2024-08-03 16:31:37 +0200

    Time series example in progress

--- scripts/introducing_TimeSeries.jl
+++ scripts/introducing_TimeSeries.jl
@@ -32,4 +32,68 @@ println("Variance:", var_val)
 # Plot the white Noise.
 plot(white_noise, title="Gaussian White Noise", xlabel="Samples", ylabel="Value", legend=false, color=:blue )
 
-#.......in progress
+# Time Series Example. 
+using TimeSeries, Dates
+
+# Create a timestamp range for the year 2024.
+timestamp = Date("2024-01-01"):Day(1):Date("2024-12-31")
+
+# Match the length of values with timestamp.
+val = randn(length(timestamp))
+
+# Create a dataframe 
+df = TimeArray(timestamp, val)
+
+# Plot the data.
+plot(df, title="Generated Data for 2024", xlabel = "Date", ylabel = "Values", legend= false, color=:blue)
+
+# Test for stationarity. 
+using HypothesisTests
+
+# Perform Augmented Dickey-Fuller Test to test for a unit root.
+test_results = ADFTest(val , :none, 1)
+
+# Plot ACF and PACF
+using Random, StatsBase
+
+# Compute ACF
+acf_values = autocor(val, 40)  # Returns ACF values for lags up to 40
+
+# Compute PACF
+pacf_val = pacf(val, 60)  # Returns PACF values for lags up to 40
+
+# Plot ACF
+plot(acf_values, title="ACF", xlabel="Lag", ylabel="ACF", legend=false)
+
+# Plot PACF
+plot(pacf_values, title="PACF", xlabel="Lag", ylabel="PACF", legend=false)
+
+# Compute ACF
+acf_values = autocor(val, demean=true)
+
+# Compute PACF
+lags = 1:40
+pacf_val = pacf(val, lags)
+
+# Plot ACF and PACF.
+acf_plot = plot(acf_values, seriestype=:bar, title="ACF")
+pacf_plot = plot(pacf_val, seriestype=:bar, title="PACF")
+
+# Display the plots
+plot(acf_plot)
+
+# Plot PACF
+plot(lags, pacf_val, title="PACF", xlabel="Lag", ylabel="PACF", legend=false)
+
+
+
+# Fit an ARIMA model
+#model = fit(ARIMA, ts)
+
+# Forecast 30 days ahead
+#forecast = predict(model, 30)
+
+# Plot the original time series and the forecast
+#plot(ts, label="Original Data", title="ARIMA Model Forecast", xlabel="Date", ylabel="Value")
+#plot!(forecast, label="Forecast", color=:red)
+
