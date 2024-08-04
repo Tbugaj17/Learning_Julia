c0ff2c8a09526d1629c291f130a832bb8424ba45
Tbugaj17 <tbugaj17@student.aau.dk>
2024-08-04 21:50:58 +0200

    added moving average

--- scripts/white_noise_example.jl
+++ scripts/white_noise_example.jl
@@ -1,9 +1,9 @@
-#White Noise Example. 
+#White Noise Example with Moving Average. 
 #Time Series.
 
 using Random, Plots, Statistics
 
-# White Noise example.
+# Setting the seed.
 Random.seed!(1234)
 
 # Generate 500 samples of Gaussian White Noise.
@@ -19,10 +19,31 @@ var_val = var(white_noise)
 println("Mean value:", mean_val)
 println("Variance:", var_val)
 
+
+# Define function moving_avarage to smoothe the white noise. 
+function moving_avarage(data, window_size)
+    return[mean(data[i:i+window_size-1]) for i in 1:length(data)-window_size+1]
+end
+
+"""
+The moving average filter calculates an avarage of the data within a sliding window (subset of data points).
+
+- mean(data[i:i+window_size-1]) # calculates the mean of a subset of data points from current position i to i + window_size-1.
+
+- for i in 1:length(data)-window_size+1 # for loop to iterate from the start of data points to where a full window can be taken.
+"""
+# Apply moving average.
+window_size = 8 # Can be adjustet.
+smoothed_noise = moving_avarage(white_noise, window_size)
+
 # Plot the white Noise.
 plot(white_noise, title="Gaussian White Noise", xlabel="Samples", ylabel="Value", legend=false, color=:blue )
 
+# Add the smoothed noise to the plot.
+plot!(smoothed_noise, color=:red, linewidth=2, label="Smoothed Noise")
 
+# Display the plot.
+display(plot)
 
 
 
