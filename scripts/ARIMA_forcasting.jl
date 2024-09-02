57c2c6868056405a5176512e80ea3078a68a8da4
Tbugaj17 <tbugaj17@student.aau.dk>
2024-09-02 21:25:06 +0200

    .

--- scripts/ARIMA_forcasting.jl
+++ scripts/ARIMA_forcasting.jl
@@ -1,4 +1,4 @@
-
+# IN PROGRESS!!
 using Pkg
 using Forecast, Plots, CSV, DataFrames
 
@@ -11,26 +11,3 @@ val = cumsum(randn(length(timestamp)))
 # Convert to a time series object
 data = val
 
-# Fit the ARIMA model (using Forecast.jl)
-model = fit(ARIMA, data, 1, 1, 1)  # ARIMA(1, 1, 1) as an example
-
-# Number of steps to forecast
-n_forecast = 30  # Forecasting for 30 days
-
-# Forecast the future values
-forecast_result = forecast(model, n_forecast)
-
-# Extract forecasted values and dates
-forecast_values = forecast_result.mean
-forecast_dates = Date("2025-01-01"):Day(1):Date("2025-01-30")
-
-# Combine the original and forecasted data
-full_dates = vcat(timestamp, forecast_dates)
-full_values = vcat(val, forecast_values)
-
-# Plot combined data
-plot(full_dates, full_values, label="Forecast with Observed Data", xlabel="Date", ylabel="Values", legend=:top, title="ARIMA Forecast")
-
-# Save to a CSV file
-forecast_df = DataFrame(Date=full_dates, Value=full_values)
-CSV.write("forecast_results.csv", forecast_df)
